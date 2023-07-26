66e1686605002cb24896b7ff2f18bc43b1ce97dc
Sgun369 <124457071+Sgun369@users.noreply.github.com>
2023-07-26 22:12:22 +0100

    betty anticipation

--- print_decimal.c
+++ print_decimal.c
@@ -3,7 +3,7 @@
  * print_number - print number
  * @args: number to print
  * print_decimal - prints a decimal.
- * Return: nember 
+ * Return: nember
  */
 int print_decimal(va_list args)
 {
@@ -11,33 +11,27 @@ int print_decimal(va_list args)
 	int number = va_arg(args, int);
 	int length = 0;
 
-    if (number == 0)
+	if (number == 0)
 	{
-        putchar('0');
-        return 1;
-    }
-
-   
-    if (number < 0)
+		putchar('0');
+		return 1;
+	}
+	if (number < 0)
 	{
-        putchar('-');
-        number = -number;
-        length++;
-    }
-
-    
-    while (number > 0) 
+		putchar('-');
+		number = -number;
+		length++;
+	}
+	while (number > 0)
 	{
-        reverse = reverse * 10 + number % 10;
-        number /= 10;
-        length++;
-    }
-
-    while (reverse > 0) 
+		reverse = reverse * 10 + number % 10;
+		number /= 10;
+		length++;
+	}
+	while (reverse > 0)
 	{
-        putchar(reverse % 10 + '0');
-        reverse /= 10;
-    }
-
-    return (length);
+		putchar(reverse % 10 + '0');
+		reverse /= 10;
+	}
+	return (length);
 }
