bcfce8e815e3757c5057cfde38fce759c8b52414
ggMBD <135523461+ggMBD@users.noreply.github.com>
2023-07-26 20:42:02 +0000

    test

--- print_decimal.c
+++ print_decimal.c
@@ -7,27 +7,5 @@
  */
 int print_decimal(va_list args)
 {
-	unsigned int name, dec, number = 1, account = 0;
-	int d;
-
-	d = va_arg(args, int);
-	if (d < 0)
-	{
-		name = -d;
-		account += _putchar('-');
-	}
-	else
-		name = d;
-	dec = name;
-	while (dec > 9)
-	{
-		dec /= 10;
-		number *= 10;
-	}
-	while (number >= 1)
-	{
-		account += _putchar(((name / number) % 10) + '0');
-		number /= 10;
-	}
-	return (accoun);
+	
 }
