4dadec269c75f6d97270076edada87d991ad8fbb
ggMBD <135523461+ggMBD@users.noreply.github.com>
2023-07-26 10:26:41 +0000

    maw

--- get_func.c
+++ get_func.c
@@ -14,9 +14,9 @@ int (*get_func(char *ch)(va_list))
 	int index;
 
 	pros_f print_op[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
 		{NULL, NULL}
 	};
 
